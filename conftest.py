05b6384136822f6a0c1d52704d227cf812a4bca3
Oleksandr <aleksandrvdidenko@gmail.com>
2025-05-04 14:41:17 +0300

    04.05.25-2

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='function')
 def driver():
     options = Options()
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     yield driver
